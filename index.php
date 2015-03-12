2bfceb8b94010f390489fdbcb4c7730754ee1a14
TEMI <temi@temi-Inspiron-1470.(none)>
2015-03-12 12:57:03 +0100

    new index.php

--- index.php
+++ index.php
@@ -1,32 +1,38 @@
 
-<<<<<<< HEAD
-=======
+<?php
+$appId = '1560387527551071'; 
+$appSecret = 'da15602cebc359bd7b49b4ff354950f0';
+$redirectUrl = 'http://localhost/projectApp/index.php'; 
+?>
 
->>>>>>> 3a0bd375ee9ab61076257e29bc6dbb1b011c8df0
 <html>
-<body><h1>
-	<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '656253677833609',
-      xfbml      : true,
-      version    : 'v2.2'
-    });
-  };
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<!-- <a id="header-logo" href="index.php" title="Retour à l'acceuil">  -->
+		<title>index</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		</head>
+			<body>
+			<P align="center">
+				<IMG src="images/quiz.jpg" width="534" height="250" alt="quiz" title="quz" vspace="5" hspace="5">
+				<BR>
+			<P> 				
+				<div>
+					<center> <a  href="http://localhost/projectApp/quiz2.php"> Start the quiz clic here !!!</a> </center>
+				</div>
+				<br><br>
+				<div class="adroite">
+				<a href="http://www.facebook.com/{FaceBook ID}">  <!-- id of wefoud404 page!-->
+				<img src="http://www.socialmediabuttons.com/images/facebook4.gif" title="By SocialMediaButtons.com" border="0" />
+				</a><br><a href="http://www.socialmediabuttons.com/"></a>
+				</div>
+				
+				  	
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
 
-<div
-  class="fb-like"
-  data-share="true"
-  data-width="450"
-  data-show-faces="true">
-</div>
-</body></html>
+	<footer id="footer">
+
+	</footer>
+	</body>
+
+</html>
