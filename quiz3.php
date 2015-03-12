a1308b01e005a1bddbedf0307f128f53ccbe97b4
TEMI <temi@temi-Inspiron-1470.(none)>
2015-03-12 12:59:48 +0100

    quiz3.php v1

---
+++ quiz3.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+	</head>
+		<body>
+		   <B>
+			   <!--<?php echo stripslashes ( $question2 ? $question2 : "Vous n'avez pas répondu, mais on vous donne tout de même la réponse, second test."); ?> !-->
+			   </B>
+				<P align="center"><IMG src="images/test2.png" width="234" height="240" alt="test2" title="test test test" vspace="5" hspace="5"><P><BR> 
+		</body>
+	
+</html>
