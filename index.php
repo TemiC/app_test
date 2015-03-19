54abcab590394a4ada71c7c8cb80f4900d3c054a
TEMI <temi@temi-Inspiron-1470.(none)>
2015-03-19 23:03:21 +0100

    v1

--- index.php
+++ index.php
@@ -1,38 +1,18 @@
-
-<?php
-$appId = '1560387527551071'; 
-$appSecret = 'da15602cebc359bd7b49b4ff354950f0';
-$redirectUrl = 'http://localhost/projectApp/index.php'; 
-?>
-
 <html>
 	<head>
+		
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-	<!-- <a id="header-logo" href="index.php" title="Retour à l'acceuil">  -->
 		<title>index</title>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		</head>
 			<body>
-			<P align="center">
-				<IMG src="images/quiz.jpg" width="534" height="250" alt="quiz" title="quz" vspace="5" hspace="5">
-				<BR>
-			<P> 				
-				<div>
-					<center> <a  href="http://localhost/projectApp/quiz2.php"> Start the quiz clic here !!!</a> </center>
-				</div>
-				<br><br>
-				<div class="adroite">
-				<a href="http://www.facebook.com/{FaceBook ID}">  <!-- id of wefoud404 page!-->
-				<img src="http://www.socialmediabuttons.com/images/facebook4.gif" title="By SocialMediaButtons.com" border="0" />
-				</a><br><a href="http://www.socialmediabuttons.com/"></a>
-				</div>
-				
-				  	
-
-
-	<footer id="footer">
-
-	</footer>
+			<div id="container">
+					<h1> WE FOUND 404 QUIZ FAN </h1>
+					<img src="images/quizzi.jpg" width="630" height="300" alt="quiz" title="quiz" vspace="5" hspace="5"/>				
+					<div>
+						<?php include('connection.php'); ?>
+					</div>
+			</div>
 	</body>
 
 </html>
