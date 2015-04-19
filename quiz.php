3c5acf8f14daaceee0e4b28db3ce46c09a0271f0
TEMI <temi@temi-Inspiron-1470.(none)>
2015-04-20 01:56:03 +0200

    SELECT

--- quiz.php
+++ quiz.php
@@ -39,7 +39,7 @@
 				}
 				else{
 					//$query = "SELECT * FROM qcm";
-					$result = $dbhandle->query('SELECT * FROM qcm');
+					$result = $dbhandle->query('SELECT * FROM qcm WHERE id = 1');
 					if (!$result) 
 						die("Cannot execute query.");
 					while ($row = $result->fetchArray()) {
