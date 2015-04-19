c07f82d82230d203e5c01a1b5d8a5cfc2e0c44e4
TEMI <temi@temi-Inspiron-1470.(none)>
2015-04-20 00:53:06 +0200

    insert multiple + SELECT test

--- MyDB.php
+++ MyDB.php
@@ -38,6 +38,8 @@ EOF;
    
    $base->exec('INSERT INTO qcm (question, rep1, rep2, rep3, rep_juste, images)VALUES("Quel était le mot remplacé par WE ?", "Nut", "Not", "Nit", "Not""images/404.jpeg")');
    echo "Row inserted \n";
+   $base->exec('INSERT INTO qcm (question, rep1, rep2, rep3, rep_juste, images)VALUES("Quest-ce que freepik ?", "Un site web super genial pour tous les webdesigners", "Un site de rencontre pour les hommes tous désespérés","Un site créé par des étudiants de la promo de Janvier de lESGI","Un site web super génial pour tous les web designers","images/freepik.jpg")');
+   echo "Row inserted \n";
 }
    /*class MyDB extends SQLite3
    {
--- quiz.php
+++ quiz.php
@@ -33,6 +33,29 @@
 					//echo 'connect database ok';
 					require_once("MyDB");
 				}*/
+				$dbhandle = sqlite_open('quizz.db', 0666, $error);
+				if (!$dbhandle) 
+					die ($error);
+				
+				$query = "SELECT * FROM qcm";
+				$result = sqlite_query($dbhandle, $query);
+				if (!$result) 
+					die("Cannot execute query.");
+				$row = sqlite_fetch_array($result, SQLITE_ASSOC); 
+				print_r($row);
+				echo "<br>";
+
+				sqlite_rewind($result);
+				$row = sqlite_fetch_array($result, SQLITE_NUM); 
+				print_r($row);
+				echo "<br>";
+
+				sqlite_rewind($result);
+				$row = sqlite_fetch_array($result, SQLITE_BOTH); 
+				print_r($row);
+				echo "<br>";
+
+				sqlite_close($dbhandle);
 				
 				$nbRecord = pg_query($connexion,"SELECT * FROM quizz.qcm");  //select all the datas comming from the DB
 				$nbRecord = pg_num_rows($nbRecord);  //nb insertions in the DB
