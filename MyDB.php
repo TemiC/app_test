910b1ea71ac8c6c250dfe902acc9f8863c979805
TEMI <temi@temi-Inspiron-1470.(none)>
2015-04-21 03:38:15 +0200

    test multiple table v7

--- MyDB.php
+++ MyDB.php
@@ -28,12 +28,51 @@ else{
 		id INTEGER PRIMARY KEY AUTOINCREMENT
 		);
 		
-		CREATE TABLE user( 
+	  CREATE TABLE user( 
 		name char(5O) NOT NULL,
 		mail char(100) NOT NULL,
 		genre char(25) NOT NULL,
 		id_fb char(100) NOT NULL,
 		id INTEGER PRIMARY KEY AUTOINCREMENT
+		);
+		
+	  CREATE TABLE qcm1( 
+		question text NOT NULL,
+		rep1 char(100) NOT NULL,
+		rep2 char(100) NOT NULL,
+		rep3 char(100) NOT NULL,
+		rep_juste char(100) NOT NULL,
+		images character(60) NOT NULL,
+		id autoincrement PRIMARY KEY NOT NULL
+		);
+		
+	   CREATE TABLE qcm2( 
+		question text NOT NULL,
+		rep1 char(100) NOT NULL,
+		rep2 char(100) NOT NULL,
+		rep3 char(100) NOT NULL,
+		rep_juste char(100) NOT NULL,
+		images character(60) NOT NULL,
+		id autoincrement PRIMARY KEY NOT NULL
+		);
+		
+	  CREATE TABLE qcm3( 
+		question text NOT NULL,
+		rep1 char(100) NOT NULL,
+		rep2 char(100) NOT NULL,
+		rep3 char(100) NOT NULL,
+		rep_juste char(100) NOT NULL,
+		images character(60) NOT NULL,
+		id autoincrement PRIMARY KEY NOT NULL
+		);
+		
+	  CREATE TABLE score( 
+		uid integer PRIMARY KEY NOT NULL,
+		score_q1 integer,
+		score_q2 integer,
+		score_q3 integer,
+		score_first integer,
+		score_second integer,
 		);
 EOF;
 
@@ -51,124 +90,8 @@ EOF;
    $base->exec($query1);
    var_dump($base);
    
-    $base->close();
-	
+   $base->close();
 	
 }
-   /*class MyDB extends SQLite3
-   {
-      function __construct()
-      {
-         $this->open('quizz.db');
-      }
-   }
-   $db = new MyDB();
-   if(!$db){
-      echo $db->lastErrorMsg();
-   } 
-   else {
-      echo "Opened database successfully\n";
-   }*/
-   
-   
- /*  
- $sql1 =<<<EOF
-      CREATE TABLE qcm1( 
-		question text NOT NULL,
-		rep1 char(100) NOT NULL,
-		rep2 char(100) NOT NULL,
-		rep3 char(100) NOT NULL,
-		rep_juste char(100) NOT NULL,
-		images character(60) NOT NULL,
-		id autoincrement PRIMARY KEY NOT NULL
-		);
-EOF;
-
- $ret1 = $db->exec($sql1);
-   if(!$ret1){
-      echo $db->lastErrorMsg();
-   } 
-   else {
-      echo "Table created successfully\n";
-   }
-
- $sql2 =<<<EOF
-      CREATE TABLE qcm2( 
-		question text NOT NULL,
-		rep1 char(100) NOT NULL,
-		rep2 char(100) NOT NULL,
-		rep3 char(100) NOT NULL,
-		rep_juste char(100) NOT NULL,
-		images character(60) NOT NULL,
-		id autoincrement PRIMARY KEY NOT NULL
-		);
-EOF;
-
- $ret2 = $db->exec($sql2);
-   if(!$ret2){
-      echo $db->lastErrorMsg();
-   } 
-   else {
-      echo "Table created successfully\n";
-   }
-
- $sql3 =<<<EOF
-      CREATE TABLE qcm3( 
-		question text NOT NULL,
-		rep1 char(100) NOT NULL,
-		rep2 char(100) NOT NULL,
-		rep3 char(100) NOT NULL,
-		rep_juste char(100) NOT NULL,
-		images character(60) NOT NULL,
-		id autoincrement PRIMARY KEY NOT NULL
-		);
-EOF;
-
- $ret3 = $db->exec($sql3);
-   if(!$ret3){
-      echo $db->lastErrorMsg();
-   } 
-   else {
-      echo "Table created successfully\n";
-   }
-
- $sql4 =<<<EOF
-      CREATE TABLE score( 
-		uid integer PRIMARY KEY NOT NULL,
-		score_q1 integer,
-		score_q2 integer,
-		score_q3 integer,
-		score_first integer,
-		score_second integer,
-		);
-EOF;
-
-
- $ret3 = $db->exec($sql3);
-   if(!$ret3){
-      echo $db->lastErrorMsg();
-   } 
-   else {
-      echo "Table created successfully\n";
-   }
-   
-   
-$sql5 =<<<EOF
-      CREATE TABLE user( 
-		name character(25) NOT NULL,
-	    mail character(50) NOT NULL,
-	    genre character(25) NOT NULL,
-	    uid serial NOT NULL,
-	    id_fb int PRIMARY KEY NOT NULL,
-		);
-EOF;
-
-   $ret5 = $db->exec($sql5);
-   if(!$ret5){
-      echo $db->lastErrorMsg();
-   } 
-   else {
-      echo "Table created successfully\n";
-   }*/
   
 ?>
