fa4645b2f57fad570d7acaa912544c5ecfdabb00
TemiC <temidayoifekoya@yahoo.fr>
2015-04-22 03:06:39 +0200

    test

--- MyDB.php
+++ MyDB.php
@@ -15,11 +15,6 @@ else{
 	//require_once("MyDB");
 	
 	$base->exec('DROP TABLE IF EXISTS qcm');
-	$base->exec('DROP TABLE IF EXISTS qcm1');
-	$base->exec('DROP TABLE IF EXISTS qcm2');
-	$base->exec('DROP TABLE IF EXISTS qcm3');
-	$base->exec('DROP TABLE IF EXISTS user');
-	$base->exec('DROP TABLE IF EXISTS score');
 	
 	$sql =<<<EOF
       CREATE TABLE qcm( 
@@ -32,52 +27,6 @@ else{
 		id INTEGER PRIMARY KEY AUTOINCREMENT
 		);
 		
-	  CREATE TABLE user( 
-		name char(5O) NOT NULL,
-		mail char(100) NOT NULL,
-		genre char(25) NOT NULL,
-		id_fb char(100) NOT NULL,
-		id INTEGER PRIMARY KEY AUTOINCREMENT
-		);
-		
-	  CREATE TABLE qcm1( 
-		question text NOT NULL,
-		rep1 char(100) NOT NULL,
-		rep2 char(100) NOT NULL,
-		rep3 char(100) NOT NULL,
-		rep_juste char(100) NOT NULL,
-		images character(60) NOT NULL,
-		id autoincrement PRIMARY KEY NOT NULL
-		);
-		
-	   CREATE TABLE qcm2( 
-		question text NOT NULL,
-		rep1 char(100) NOT NULL,
-		rep2 char(100) NOT NULL,
-		rep3 char(100) NOT NULL,
-		rep_juste char(100) NOT NULL,
-		images character(60) NOT NULL,
-		id autoincrement PRIMARY KEY NOT NULL
-		);
-		
-	  CREATE TABLE qcm3( 
-		question text NOT NULL,
-		rep1 char(100) NOT NULL,
-		rep2 char(100) NOT NULL,
-		rep3 char(100) NOT NULL,
-		rep_juste char(100) NOT NULL,
-		images character(60) NOT NULL,
-		id autoincrement PRIMARY KEY NOT NULL
-		);
-		
-	  CREATE TABLE score( 
-		uid integer PRIMARY KEY NOT NULL,
-		score_q1 integer,
-		score_q2 integer,
-		score_q3 integer,
-		score_first integer,
-		score_second integer,
-		);
 		
 		
 		
