f172c93c1385b4783cfd59594197d55186654704
TemiC <temidayoifekoya@yahoo.fr>
2015-04-22 20:32:37 +0200

    test

--- connection.php
+++ connection.php
@@ -2,37 +2,54 @@
 
 	require_once("session.php"); 
 	
-	if($session){
-		try{
-			$_SESSION['fb_token'] =(string) $session->getAccessToken();
-			$request = new FacebookRequest($session, 'GET', '/me');
-			$response = $request->execute();
-			$user_profile = $response->getGraphObject("Facebook\GraphUser");
-
-			$request = new FacebookRequest(
-			  $session,
-			  'GET',
-			  '/me'
-			);
-			$response = $request->execute();
-			$graphObject = $response->getGraphObject()->asArray();
-
-            $id = $user_profile->getId();
-            $fname = $user_profile->getfirstName();
-            $lname = $user_profile->getlastName();
-            $email = $user_profile->getProperty('mail');
-            $gender = $user_profile->getProperty('gender'); 
-
-            
+	if(isset($session)) {
+		try {	
+			$user_profile = (new FacebookRequest(
+			$session, 'GET', '/me'
+			))->execute()->getGraphObject(GraphUser::className());
+		} 
+		catch(FacebookRequestException $e) {
+		} 
+		
+		require_once("MyDB.php");
+		$_SESSION['fb_token'] = (string) $session->getAccessToken();
+		$user_profile = (new \Facebook\FacebookRequest($session, 'GET', '/me'))->execute()->getGraphObject(Facebook\GraphUser::className());  //get user informations 
+		$name = $user_profile->getProperty('name');
+		$mail = $user_profile->getProperty('email');
+		$genre = $user_profile->getProperty('gender');
+		$id_fb = $user_profile->getProperty('id');
+		
+		$_SESSION['name'] = $name;
+		$_SESSION['mail'] = $mail;
+		$_SESSION['genre'] = $genre;
+		$_SESSION['id_fb'] = $id_fb;
+		
+		
+	
+	function ckeckUser($id_fb, $genre, $mail, $name){
+		$dbhandle = new SQLite3('quizz.db');
+		if (!$dbhandle){
+			die ('error came');
 		}
-		catch(FacebookRequestException $e){
-			echo "Erreur ". $e->getMessage();
-			//On supprime la variable de session au cas ou
-			session_destroy();
+		$query = $dbhandle->query('SELECT * FROM user WHERE id_fb ="$id_fb"');
+		if (!$query) 
+			die("Cannot execute query.");
+		while ($row = $query->fetchArray()) {
+			if(empty($query)){
+			$query = 'INSERT INTO user( name, mail, genre, id_fb) VALUES("$name","$mail", "$genre","$id_fb")';
+			$dbhandle->exec($query);
+			var_dump($dbhandle);
+			}
+			else{
+				$query = 'UPDATE user SET name ="$name", mail = "$mail", genre = "$genre", WHERE id_fb = "$id_fb"';
+				$dbhandle->exec($query);
+			}
 		}
 		
+	}
+	}
 	else {
-		$url = $helper->getLoginUrl(['email','user_photos']);
-		echo "Veuillez vous connecter en <a href='".$url."'>cliquant ici</a>";
+		$loginUrl = $helper->getLoginUrl($permission);
+		echo '<a href="'.$loginUrl.'">Click here to !!</a>';
 	}
 ?>
