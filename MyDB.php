87a458f9a772e0632aeec2feeb1a851d5b01c3d2
TemiC <temidayoifekoya@yahoo.fr>
2015-04-22 13:02:00 +0200

    test

--- MyDB.php
+++ MyDB.php
@@ -90,7 +90,7 @@ EOF;
    else {
       echo "All table created successfully\n";
       $query = ' INSERT INTO qcm (question, rep1, rep2, rep3, rep_juste, images)VALUES("Quel était le mot remplacé par WE ?", "Nut", "Not", "Nit", "Not", "images/404.jpeg");
-   				 INSERT INTO qcm (question, rep1, rep2, rep3, rep_juste, images)VALUES("Quel était le mot remplacé par WE ?", "Nut", "Not", "Nit", "Not", "images/404.jpeg");';
+   				 INSERT INTO qcm (question, rep1, rep2, rep3, rep_juste, images)VALUES("test ?", "test", "test", "test", "test", "images/404.jpeg");';
       //$result = $dbhandle->query('SELECT * FROM qcm');
 	  $result = $base->exec($query);
 	  var_dump($base);
