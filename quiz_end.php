0c524be88bed65b82cac09debec068e80cc35e3b
TemiC <temidayoifekoya@yahoo.fr>
2015-04-22 20:05:50 +0200

    test

--- quiz_end.php
+++ quiz_end.php
@@ -26,22 +26,15 @@
 
 
 
-			<?php 
-
-			 // recupération des commentaires d'un article 
-				function getCountLikeFacebook($page){
-					$page = "https://www.facebook.com/wefound404?fref=nf";
-				    $url = "https://api.facebook.com/method/links.getStats?urls=".urlencode($page)."&format=json";
-				    $data = json_decode(file_get_contents($url));
-				 
-				    if(!isset($data[0]->like_count)){
-				    	return 'erreur';
-				    }
-				 
-				    return $data[0]->like_count;
-				    echo "$data";
-				}
+			<?php
+				$api = file_get_contents('http://graph.facebook.com/[https://www.facebook.com/wefound404?fref=nf]'); // On récupère la page de l'API facebook
+				$likes = json_decode($api); // On décode le JSON renvoyé
+				$likes = $likes->likes; // On récupère la valeur de "likes"
+				$likes = (string)$likes; // On en fait une chaîne de caractères
+				echo $likes; // On l'affiche
 			?>
+
+		
 			<script>
 			  window.fbAsyncInit = function() {
 				FB.init({
