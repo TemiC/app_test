9e71afac4e080ae539958692227171d336142da0
TemiC <temidayoifekoya@yahoo.fr>
2015-04-23 14:24:00 +0200

    test

--- identity.php
+++ identity.php
@@ -75,7 +75,7 @@
                       version    : 'v2.3'
                     });
                   };
-
+                  </script>
                  <div id="fb-root"></div>
                   <script>(function(d, s, id) {
                     var js, fjs = d.getElementsByTagName(s)[0];
