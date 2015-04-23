3b5d651b0d232bf49ce8800680f19550a45a7dd9
TemiC <temidayoifekoya@yahoo.fr>
2015-04-23 12:55:29 +0200

    test

---
+++ connectionSave.php
@@ -0,0 +1,34 @@
+
+<?php
+
+
+require_once("session.php"); 
+
+if(isset($session)) {
+		try {	
+			$user_profile = (new FacebookRequest(
+			$session, 'GET', '/me'
+			))->execute()->getGraphObject(GraphUser::className());
+		} 
+		catch(FacebookRequestException $e) {
+		} 
+		
+		require_once("MyDB.php");
+		$_SESSION['fb_token'] = (string) $session->getAccessToken();
+		$user_profile = (new \Facebook\FacebookRequest($session, 'GET', '/me'))->execute()->getGraphObject(Facebook\GraphUser::className());  //get user informations 
+		$name = $user_profile->getProperty('lname');
+		//echo "$name";
+		//exit(0);
+		$name = $user_profile->getProperty('fname');
+		$mail = $user_profile->getProperty('mail');
+		$genre = $user_profile->getProperty('gendre');
+		$id_fb = $user_profile->getProperty('id');
+		
+		$_SESSION['fname'] = $fname;
+		$_SESSION['lname'] = $lname;
+		$_SESSION['mail'] = $mail;
+		$_SESSION['genre'] = $genre;
+		$_SESSION['id_fb'] = $id_fb;
+}
+
+?>
--- quiz.php
+++ quiz.php
@@ -5,6 +5,7 @@
 	ini_set('display_errors', 1);
 	// Afficher les erreurs et les avertissements
 	error_reporting(e_all);
+	require_once("connectionSave.php");
 ?>
 
 
