af5ce49b54a28f08c20383def7e5f6c6eb5a6401
TemiC <temidayoifekoya@yahoo.fr>
2015-04-23 14:47:50 +0200

    test

--- index.php
+++ index.php
@@ -23,29 +23,29 @@
 					<br>
 					<img src="images/quizzi.jpg" width="430" height="250" alt="quiz" title="quiz" vspace="5" hspace="5"/>	
 					<script>
-			  window.fbAsyncInit = function() {
-				FB.init({
-				  appId      : '656253677833609',
-				  xfbml      : true,
-				  version    : 'v2.2'
-				});
-			  };
+					  window.fbAsyncInit = function() {
+						FB.init({
+						  appId      : '656253677833609',
+						  xfbml      : true,
+						  version    : 'v2.2'
+						});
+					  };
 
-			  (function(d, s, id){
-				 var js, fjs = d.getElementsByTagName(s)[0];
-				 if (d.getElementById(id)) {return;}
-				 js = d.createElement(s); js.id = id;
-				 js.src = "//connect.facebook.net/en_US/sdk.js";
-				 fjs.parentNode.insertBefore(js, fjs);
-			   }(document, 'script', 'facebook-jssdk'));
-			</script>
+					  (function(d, s, id){
+						 var js, fjs = d.getElementsByTagName(s)[0];
+						 if (d.getElementById(id)) {return;}
+						 js = d.createElement(s); js.id = id;
+						 js.src = "//connect.facebook.net/en_US/sdk.js";
+						 fjs.parentNode.insertBefore(js, fjs);
+					   }(document, 'script', 'facebook-jssdk'));
+					</script>
 
-			<div
-			  class="fb-like"
-			  data-share="true"
-			  data-width="450"
-			  data-show-faces="true">
-			</div>
+					<div
+					  class="fb-like"
+					  data-share="true"
+					  data-width="450"
+					  data-show-faces="true">
+					</div>
 
 				    
 					<div>	
