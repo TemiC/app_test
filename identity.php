2539cc25848c8dc960d26018e694d9fe9c744a92
TemiC <temidayoifekoya@yahoo.fr>
2015-04-23 15:02:19 +0200

    test

--- identity.php
+++ identity.php
@@ -81,15 +81,14 @@
                   });
                 };
                 </script>
-                  <script>
-                  (function(d, s, id) {
-                    var js, fjs = d.getElementsByTagName(s)[0];
-                    if (d.getElementById(id)) return;
-                    js = d.createElement(s); js.id = id;
-                    js.src = "http://hidden-retreat-3686.herokuapp.com/identity.php";
-                    fjs.parentNode.insertBefore(js, fjs);
-                  }(document, 'script', 'facebook-jssdk'));
-                  </script>
+                <script>(function(d, s, id) {
+                  var js, fjs = d.getElementsByTagName(s)[0];
+                  if (d.getElementById(id)) return;
+                  js = d.createElement(s); js.id = id;
+                  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.3&appId=656253677833609";
+                  fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
+                </script>
 
                   <div class="fb-comments" data-href="http://hidden-retreat-3686.herokuapp.com/identity.php" data-numposts=3 data-colorschema="light"> </div>
                 </td>
