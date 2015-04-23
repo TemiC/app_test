890e29efc026a034401fe836f0bb7ea8e67d30d7
TemiC <temidayoifekoya@yahoo.fr>
2015-04-23 14:35:05 +0200

    test

--- identity.php
+++ identity.php
@@ -18,6 +18,7 @@
 
     <body>
     <div id="fb-root"></div>
+     
      <script>
       window.fbAsyncInit = function() {
         FB.init({
@@ -80,7 +81,8 @@
                 <td>Commentaire</td>
                 <td>
 
-                  <script>(function(d, s, id) {
+                  <script>
+                  (function(d, s, id) {
                     var js, fjs = d.getElementsByTagName(s)[0];
                     if (d.getElementById(id)) return;
                     js = d.createElement(s); js.id = id;
