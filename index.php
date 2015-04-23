362361a4a50ae1487beb35b78c11be6100d7e714
TemiC <temidayoifekoya@yahoo.fr>
2015-04-23 12:30:46 +0200

    test

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+
+	session_start();
+	// Afficher les erreurs à l'écran
+	ini_set('display_errors', 1);
+	// Enregistrer les erreurs dans un fichier de log
+	ini_set('log_errors', 1);
+	// Nom du fichier qui enregistre les logs (attention aux droits à l'écriture)
+	ini_set('error_log', dirname(__file__) . '/log_error_php.txt');
+	// Afficher les erreurs et les avertissements
+	error_reporting(e_all);
+?>
+
+
 <html>
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
